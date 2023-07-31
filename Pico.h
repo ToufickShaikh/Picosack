6b57d2fd14ef77621272ce571ef8222614d515e7
Toufick190 <97280255+Toufick190@users.noreply.github.com>
2023-07-31 08:38:26 +0530

    Create Pico.h

---
+++ Pico.h
@@ -0,0 +1,77 @@
+String html = R"***(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Toufick Shaikh - Web Developer Portfolio</title>
+  <style>
+    /* Your custom CSS styles go here */
+    body {
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
+      margin: 0;
+      padding: 0;
+    }
+    /* Add your styles for header, navigation, sections, etc. */
+  </style>
+</head>
+<body>
+  <header>
+    <!-- Add a header with Toufick Shaikh's name and maybe a logo -->
+    <h1>Toufick Shaikh</h1>
+    <nav>
+      <!-- Add a navigation menu with links to different sections of the portfolio -->
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section id="about">
+    <!-- Add information about Toufick Shaikh, his skills, and experience -->
+    <h2>About Me</h2>
+    <p>
+      Hi, I'm Toufick Shaikh, a passionate web developer with a strong focus on creating beautiful and functional websites. I have experience in front-end and back-end development, and I enjoy working with the latest web technologies to build innovative solutions.
+    </p>
+    <!-- You can also add a photo of Toufick here -->
+  </section>
+
+  <section id="projects">
+    <!-- Showcase some of Toufick Shaikh's notable web development projects -->
+    <h2>Projects</h2>
+    <div class="project">
+      <h3>Project 1</h3>
+      <p>
+        Description of the project. Highlight the technologies used and your contributions to the project's success.
+      </p>
+      <!-- Add images, links, or live demos of the projects -->
+    </div>
+    <div class="project">
+      <h3>Project 2</h3>
+      <p>
+        Description of the project. Highlight the technologies used and your contributions to the project's success.
+      </p>
+      <!-- Add images, links, or live demos of the projects -->
+    </div>
+    <!-- Add more projects as needed -->
+  </section>
+
+  <section id="contact">
+    <!-- Provide contact information or a contact form -->
+    <h2>Contact</h2>
+    <p>
+      You can reach me via email at <a href="mailto:your-email@example.com">your-email@example.com</a> or connect with me on LinkedIn: <a href="https://www.linkedin.com/in/your-linkedin-profile">linkedin.com/in/your-linkedin-profile</a>
+    </p>
+    <!-- You can also add a contact form if desired -->
+  </section>
+
+  <footer>
+    <!-- Add footer information and copyright notice -->
+    <p>&copy; 2023 Toufick Shaikh. All rights reserved.</p>
+  </footer>
+</body>
+</html>
+)***";
