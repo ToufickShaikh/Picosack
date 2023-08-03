2408ab016902c93df4ef6bebcae19d224873c5aa
Toufick Shaikh <97280255+Toufick190@users.noreply.github.com>
2023-08-03 08:06:13 +0530

    Update Pico.h
    new root page

--- Pico.h
+++ Pico.h
@@ -5,73 +5,122 @@ String html = R"***(
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Toufick Shaikh - Web Developer Portfolio</title>
-  <style>
-    /* Your custom CSS styles go here */
-    body {
-      font-family: Arial, sans-serif;
-      line-height: 1.6;
-      margin: 0;
-      padding: 0;
-    }
-    /* Add your styles for header, navigation, sections, etc. */
-  </style>
+  <!-- Include the Tailwind CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-  <header>
-    <!-- Add a header with Toufick Shaikh's name and maybe a logo -->
-    <h1>Toufick Shaikh</h1>
-    <nav>
-      <!-- Add a navigation menu with links to different sections of the portfolio -->
-      <ul>
+<body class="bg-gray-100">
+  <header class="bg-blue-500 py-4">
+  <div class="container mx-auto px-4">
+    <h1 class="text-white text-3xl font-bold">Toufick Shaikh</h1>
+    <nav class="mt-4">
+      <ul class="flex space-x-4 text-white text-lg font-medium">
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="/form">Contact</a></li>
       </ul>
     </nav>
-  </header>
-
-  <section id="about">
-    <!-- Add information about Toufick Shaikh, his skills, and experience -->
-    <h2>About Me</h2>
-    <p>
+  </div>
+</header>
+<section id="about" class="py-12">
+  <div class="container mx-auto px-4">
+    <h2 class="text-2xl font-semibold mb-4">About Me</h2>
+    <p class="text-gray-800">
       Hi, I'm Toufick Shaikh, a passionate web developer with a strong focus on creating beautiful and functional websites. I have experience in front-end and back-end development, and I enjoy working with the latest web technologies to build innovative solutions.
     </p>
     <!-- You can also add a photo of Toufick here -->
-  </section>
-
-  <section id="projects">
-    <!-- Showcase some of Toufick Shaikh's notable web development projects -->
-    <h2>Projects</h2>
-    <div class="project">
-      <h3>Project 1</h3>
-      <p>
-        Description of the project. Highlight the technologies used and your contributions to the project's success.
-      </p>
-      <!-- Add images, links, or live demos of the projects -->
+  </div>
+</section>
+<section id="projects" class="py-12 bg-white">
+  <div class="container mx-auto px-4">
+    <h2 class="text-2xl font-semibold mb-8">Projects</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div class="border rounded-lg p-6">
+        <h3 class="text-lg font-semibold mb-2">Project 1</h3>
+        <p class="text-gray-800">
+          Description of the project. Highlight the technologies used and your contributions to the project's success.
+        </p>
+        <!-- Add images, links, or live demos of the projects -->
+      </div>
+      <div class="border rounded-lg p-6">
+        <h3 class="text-lg font-semibold mb-2">Project 2</h3>
+        <p class="text-gray-800">
+          Description of the project. Highlight the technologies used and your contributions to the project's success.
+        </p>
+        <!-- Add images, links, or live demos of the projects -->
+      </div>
+      <!-- Add more projects as needed -->
     </div>
-    <div class="project">
-      <h3>Project 2</h3>
-      <p>
-        Description of the project. Highlight the technologies used and your contributions to the project's success.
-      </p>
-      <!-- Add images, links, or live demos of the projects -->
-    </div>
-    <!-- Add more projects as needed -->
-  </section>
+  </div>
+</section>
+<footer class="bg-blue-500 py-4 text-white text-center">
+  <p>&copy; 2023 Toufick Shaikh. All rights reserved.</p>
+</footer>
 
-  <section id="contact">
-    <!-- Provide contact information or a contact form -->
-    <h2>Contact</h2>
-    <p>
-      You can reach me via email at <a href="mailto:your-email@example.com">your-email@example.com</a> or connect with me on LinkedIn: <a href="https://www.linkedin.com/in/your-linkedin-profile">linkedin.com/in/your-linkedin-profile</a>
-    </p>
-    <!-- You can also add a contact form if desired -->
-  </section>
-
-  <footer>
-    <!-- Add footer information and copyright notice -->
-    <p>&copy; 2023 Toufick Shaikh. All rights reserved.</p>
-  </footer>
 </body>
 </html>
+
+
+)***";
+String html = R"***(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Toufick Shaikh - Web Developer Portfolio</title>
+  <!-- Include the Tailwind CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+  <header class="bg-blue-500 py-4">
+  <div class="container mx-auto px-4">
+    <h1 class="text-white text-3xl font-bold">Toufick Shaikh</h1>
+    <nav class="mt-4">
+      <ul class="flex space-x-4 text-white text-lg font-medium">
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="/form">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+<section id="about" class="py-12">
+  <div class="container mx-auto px-4">
+    <h2 class="text-2xl font-semibold mb-4">About Me</h2>
+    <p class="text-gray-800">
+      Hi, I'm Toufick Shaikh, a passionate web developer with a strong focus on creating beautiful and functional websites. I have experience in front-end and back-end development, and I enjoy working with the latest web technologies to build innovative solutions.
+    </p>
+    <!-- You can also add a photo of Toufick here -->
+  </div>
+</section>
+<section id="projects" class="py-12 bg-white">
+  <div class="container mx-auto px-4">
+    <h2 class="text-2xl font-semibold mb-8">Projects</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div class="border rounded-lg p-6">
+        <h3 class="text-lg font-semibold mb-2">Project 1</h3>
+        <p class="text-gray-800">
+          Description of the project. Highlight the technologies used and your contributions to the project's success.
+        </p>
+        <!-- Add images, links, or live demos of the projects -->
+      </div>
+      <div class="border rounded-lg p-6">
+        <h3 class="text-lg font-semibold mb-2">Project 2</h3>
+        <p class="text-gray-800">
+          Description of the project. Highlight the technologies used and your contributions to the project's success.
+        </p>
+        <!-- Add images, links, or live demos of the projects -->
+      </div>
+      <!-- Add more projects as needed -->
+    </div>
+  </div>
+</section>
+<footer class="bg-blue-500 py-4 text-white text-center">
+  <p>&copy; 2023 Toufick Shaikh. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
+
+
 )***";
